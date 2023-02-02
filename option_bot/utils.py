9211d749a50c503f9ec46c62f127441ca9994f6e
Jody Hirschi <37758071+jhirschibar@users.noreply.github.com>
2023-02-02 10:17:16 -0500

    update utils with typing

--- option_bot/utils.py
+++ option_bot/utils.py
@@ -7,7 +7,7 @@ def timestamp_to_datetime(timestamp: int, msec_units: bool = True) -> datetime:
     return datetime.fromtimestamp(timestamp / 1000) if msec_units else datetime.fromtimestamp(timestamp)
 
 
-def first_weekday_of_month(year_month_array: np.ndarray):
+def first_weekday_of_month(year_month_array: np.ndarray)-> np.ndarray:
     if year_month_array.dtype != np.datetime64:
         year_month_array = year_month_array.astype(np.datetime64)
     return np.busday_offset(year_month_array, 0, roll="modifiedpreceding", weekmask=[1, 1, 1, 1, 1, 0, 0])
