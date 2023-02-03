77860d8306c3cd12134d299bc9e3c0f0ca414bba
Jody Hirschi <jhirschibar@gmail.com>
2023-02-03 11:22:40 -0500

    formatting fixes

--- option_bot/db_manager.py
+++ option_bot/db_manager.py
@@ -1,7 +1,13 @@
-from models import *
+# from models import *
+
 
 class PostgresPull(object):
     pass
 
+
 class PostgresPush(object):
+    pass
+
+
+class add_stock(PostgresPull):
     pass
--- option_bot/orchestrator.py
+++ option_bot/orchestrator.py
@@ -1,8 +1,10 @@
-from polygon import HistoricalOptionsPrices, OptionsContracts
+# from polygon import HistoricalOptionsPrices, OptionsContracts
 
 
 def add_ticker_to_universe():
     """Chain together the following process:
-    Pull ticker data, pull ticker price data, save price data, analyze ticker info, pass output to Options queries, pull options tickers, pull options prices, save tickers and prices, calculate greeks
+    Pull ticker data, pull ticker price data, save price data, analyze ticker info,\
+    pass output to Options queries, pull options tickers, pull options prices, \
+    save tickers and prices, calculate greeks
     """
     return
