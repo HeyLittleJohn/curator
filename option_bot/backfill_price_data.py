8cbbb7567aef63ec6562a085c153253519fa6597
Jody Hirschi <jhirschibar@gmail.com>
2023-02-03 10:29:40 -0500

    initial CLI and stock price objects

---
+++ option_bot/backfill_price_data.py
@@ -0,0 +1,54 @@
+import argparse
+from datetime import datetime, timedelta
+
+from polygon import HistoricalOptionsPrices, OptionsContracts
+
+DEFAULT_DAYS = 120
+DEFAULT_MONTHS = 24
+
+
+def main():
+    """primary CLI for adding underlying stocks to our data universe, and specifying how far back to pull data"""
+
+    parser = argparse.ArgumentParser(
+        description="CLI for adding stocks to the data pull process and for refreshing stock/options pricing data"
+    )
+
+    parser.add_argument(
+        "tickers",
+        type=str,
+        nargs="*",
+        metavar="underlying tickers",
+        default=None,
+        help="Adds underlying tickers to our options data pull universe",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--start-date",
+        type=str,
+        nargs=1,
+        metavar="YYYY-MM",
+        help="YYYY-MM formatted date str indicating start of data pull for s",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--price-days",
+        type=int,
+        nargs=1,
+        default=DEFAULT_DAYS,
+        metavar="num_days",
+        help="Int specifying number of price days wanted for each options contract",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--remove",
+        help="Removes the specified underlying ticker(s) from our options data pull universe",
+    )
+
+
+    args = parser.parse_args()
+
+    if args.tickers is not None:
