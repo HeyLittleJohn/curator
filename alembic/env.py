122c8e00ee3a225cb5c40395d4a5a19445ad2bdb
jhirschibar <jhirschibar@gmail.com>
2023-02-16 08:58:50 -0500

    feat: set up alembic and initial db migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,24 +1,101 @@
+import asyncio
 import os
+from logging.config import fileConfig
 
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from alembic import context
 from option_bot.schemas import Base
 
 
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
 
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
 
 
 def _get_url() -> str:
+    """requires a PGPASSFILE env variable directing to a .pgpass file"""
     with open(os.environ.get("PGPASSFILE"), "r") as f:
-        host, port, dbname, user, password = f.read().split(":")
+        host, port, dbname, user, password = f.read().strip().split(":")
         database_uri = f"postgresql+psycopg://{user}:{password}@{host}:{port}/{dbname}"
     return database_uri
 
 
 def create_engine_(async_engine: bool = False):
+    """no need for async connection to run migrations"""
     database_uri = _get_url()
     return create_async_engine(database_uri) if async_engine else create_engine(database_uri)
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = _get_url()
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        compare_type=True,
+        compare_server_default=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(
+        connection=connection, target_metadata=target_metadata, compare_type=True, compare_server_default=True
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = create_async_engine(_get_url())
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    asyncio.run(run_migrations_online())
