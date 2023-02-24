d552c430ad2798d649c7a08515fb467f02008ec5
jhirschibar <jhirschibar@gmail.com>
2023-02-24 17:41:27 -0500

    feat: starting option price pulls

--- option_bot/game_environment.py
+++ option_bot/game_environment.py
@@ -11,3 +11,29 @@ class GameEnvironmnet(object):
 
     def step():
         pass
+
+
+# NOTE: this could be used for the logic of pulling the prices to be considered for each step
+"""    Attributes:
+        options_tickers: List[str]
+            the options contract tickers
+
+        exp_date: [datetime, datetime]
+            the range of option expiration dates to be queried
+
+        strike_price: decimal
+            the strike price range want to include in our queries
+
+    Note:
+        exp_date and strike_price are inclusive ranges"""
+
+"""
+    Attributes:
+        ticker: str
+            the underlying stock ticker
+        base_date: [datetime]
+            the date that is the basis for current observations. \
+            In other words: the date at which you are looking at the chain of options data
+        current_price: decimal
+            The current price of the underlying ticker
+    """
