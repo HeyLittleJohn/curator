44111316be6a69e7318e4839704712d13097e829
jhirschibar <jhirschibar@gmail.com>
2023-06-29 08:05:23 -0400

    refactor: orchestration and uploader planned

--- option_bot/data_pipeline/uploader.py
+++ option_bot/data_pipeline/uploader.py
@@ -1,5 +1,8 @@
 from typing import Awaitable
 
+# from aiomultiprocess import Pool
+from data_pipeline.polygon_utils import PolygonPaginator
+
 from option_bot.proj_constants import log
 
 
@@ -58,3 +61,38 @@ class Uploader:
         pass
         # Note: you may not need this. Cleaning could remain polygon_utils file
         # or this could be abstracted to dedupe all the data types
+
+
+async def etl_pool_uploader(
+    paginator: PolygonPaginator, upload_func: Awaitable, record_size: int, expected_args: int, pool_kwargs: dict = {}
+):
+    """This function will create a process pool to concurrently upload the downloaded json to the db
+
+    Args:
+        upload_func: function to upload the data to the db, matching the endpoint/data type being queried
+        record_size: number of fields per record, used to estimate the batch size
+        expected_args: number of arguments expected to be passed to the pool. Based on files in directory
+
+    """
+    # NOTE: uploader was originally a linear class. It can't handle concurrency. Need to change
+    # uploader = Uploader(upload_func, expected_args, record_size)
+    pass
+
+
+async def upload_stock_metadata(tickers, all_):
+    """This function uploads stock metadata to the database"""
+    pass
+
+
+async def upload_options_contracts(tickers, all_):
+    """This function uploads options contract data to the database"""
+    pass
+
+
+async def upload_options_prices(tickers, all_):
+    """This function uploads options prices data to the database"""
+    pass
+
+
+def generate_directory_args(directory: str, file_type: str):
+    pass
