1a4b14febcde86d098acb9dbd102f37a942794bb
jhirschibar <jhirschibar@gmail.com>
2023-07-18 08:20:32 -0400

    feat: added game env queries

--- option_bot/rl_agent/queries.py
+++ option_bot/rl_agent/queries.py
@@ -1,16 +1,13 @@
 from db_tools.schemas import (
     OptionsPricesRaw,
-    OptionsTickerModel,
     OptionsTickers,
     StockPricesRaw,
     StockTickers,
-    TickerModel,
 )
-from sqlalchemy import delete, select, update
-from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from option_bot.utils import Session, two_years_ago
+from option_bot.utils import Session
 
 
 @Session
@@ -33,15 +30,49 @@ async def lookup_ticker_id(session: AsyncSession, ticker_str: str, stock: bool =
 
 
 @Session
-async def extract_ticker_price(session: AsyncSession, ticker: str, start_date):
-    pass
+async def extract_ticker_price(session: AsyncSession, ticker: str):
+    stmt = (
+        select(
+            StockPricesRaw.close_price,
+            StockPricesRaw.as_of_date,
+            StockPricesRaw.volume,
+            StockPricesRaw.number_of_transactions,
+        )
+        .join(StockTickers)
+        .where(StockTickers.ticker == ticker)
+    )
+    return (await session.execute(stmt)).all()
 
 
 @Session
 async def extract_options_contracts(session: AsyncSession, ticker: str, start_date):
-    pass
+    stmt = (
+        select(
+            OptionsTickers.options_ticker,
+            OptionsTickers.expiration_date,
+            OptionsTickers.strike_price,
+            OptionsTickers.contract_type,
+            OptionsTickers.shares_per_contract,
+        )
+        .join(StockTickers)
+        .where(StockTickers.ticker == ticker)
+        .where(OptionsTickers.expiration_date >= start_date)
+    )
+    return (await session.execute(stmt)).all()
 
 
 @Session
-async def extract_options_prices(session: AsyncSession, o_tickers: list[str], start_date):
-    pass
+async def extract_options_prices(session: AsyncSession, ticker: str, start_date):
+    stmt = (
+        select(
+            OptionsPricesRaw.as_of_date,
+            OptionsPricesRaw.close_price,
+            OptionsPricesRaw.volume,
+            OptionsPricesRaw.number_of_transactions,
+        )
+        .join(OptionsTickers)
+        .join(StockTickers)
+        .where(StockTickers.ticker == ticker)
+        .where(OptionsPricesRaw.as_of_date >= start_date)
+    )
+    return (await session.execute(stmt)).all()
