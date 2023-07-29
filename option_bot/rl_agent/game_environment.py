15db57edd7c7c9c3258c5159baa962fa21ab9513
jhirschibar <jhirschibar@gmail.com>
2023-07-29 13:01:12 -0400

    feat: working on prepping state

--- option_bot/rl_agent/game_environment.py
+++ option_bot/rl_agent/game_environment.py
@@ -1,6 +1,8 @@
 import asyncio
 from datetime import datetime
+import random
 
+import pandas as pd
 from py_vollib_vectorized.implied_volatility import vectorized_implied_volatility
 from py_vollib_vectorized.api import price_dataframe
 
@@ -14,15 +16,33 @@ from rl_agent.constants import DAYS_TIL_EXP, ANNUAL_TRADING_DAYS, RISK_FREE
 from option_bot.utils import trading_days_in_range
 
 
-class GameEnvironmnet(object):
-    def __init__(self, underlying_ticker: str, start_date: str, days_to_exp: int = DAYS_TIL_EXP):
+class GameEnvironment(object):
+    """The game environment for the reinforcement learning agent.
+    This env will support a single underlying ticker and up to 4 options positions
+    Supported actions: close, hold position (2)
+    The action space will be the number of combinations of actions per position. 2^n where n is [1,4]
+    """
+
+    positions = {
+        1: ["call", "put"],
+        2: ["call_credit_spread", "put_credit_spread", "staddle", "strangle"],
+        3: ["double_call_credit_spread", "double_put_credit_spread", "strap", "strip"],
+        4: ["iron_condor", "butterfly_spread"],
+    }
+    long_short = ["long", "short"]
+
+    def __init__(self, underlying_ticker: str, start_date: str, days_to_exp: int = DAYS_TIL_EXP, num_positions=1):
         self.ticker = underlying_ticker
         self.start_date = datetime.strptime(start_date, "%Y-%m-%d") if type(start_date) == str else start_date
+        self.start_days_to_exp = days_to_exp
         self.days_to_exp = days_to_exp
+        self.num_positions = num_positions
         self.end = False
+        self.state_data_df = pd.DataFrame()
+        self.underlying_price_df = pd.DataFrame()
 
     # NOTE: should this only pull for the current game and be re-called at "reset"?
-    async def pull_game_price_data(self):
+    async def _pull_game_price_data(self):
         # return await asyncio.gather(
         #     extract_ticker_price(self.ticker),
         #     extract_options_contracts(self.ticker, self.start_date),
@@ -34,7 +54,7 @@ class GameEnvironmnet(object):
 
     async def prepare_state_data(self):
         # s_price, o_contracts, o_prices = await self.pull_game_price_data()
-        df = await self.pull_game_price_data()
+        df = await self._pull_game_price_data()
         df["flag"] = df["contract_type"].apply(lambda x: "c" if x.value == "call" else "p")
         # calc the time to expiration
         df["T"] = df.apply(
@@ -42,6 +62,8 @@ class GameEnvironmnet(object):
             axis=1,
         )  # NOTE: THIS IS SLOW! Need to optimize with cuDF or np.vectorize
         # reference: https://shubhanshugupta.com/speed-up-apply-function-pandas-dataframe/#3-rapids-cudf-
+        # the two .apply()s are both slow, especially together
+
         # add the risk free rate
         df = df.merge(RISK_FREE, on="as_of_date")
         # calc the div yield
@@ -59,23 +81,36 @@ class GameEnvironmnet(object):
             inplace=True,
         )
 
-        return df
+        self.state_data_df = df
+        self.underlying_price_df = df[["as_of_date", "stock_close_price"]].drop_duplicates(ignore_index=True)
 
     def _impute_missing_data(self):
         # use this https://github.com/rsheftel/pandas_market_calendars to find missing days
         pass
 
     def reset(self):
+        self.days_to_exp = self.start_days_to_exp
+
+    def step(self):
+        """returns the next state, reward, and whether the game is over"""
+        self.days_to_exp -= 1
+
+    def _init_random_positions(self):
+        """this function initializes the game with random positions.
+        It chooses a row from the self.underlying_price_df that is atleast self.days_to_exp positions away from the last row.
+        It takes the as_of_date value and the stock_close_price from that row.
+        It then filters the self.state_data_df to only include rows with that as_of_date and chooses self.num_positions options contracts whose strike prices are +/- 8 contracts away from the stock_close_price.
+        """
+        ix = random.randint(0, len(self.underlying_price_df) - self.days_to_exp)
+        start_date = self.underlying_price_df.loc[ix, "as_of_date"]
+        under_start_price = self.underlying_price_df.loc[ix, "stock_close_price"]
+        self.state_data_df[self.state_data_df["as_of_date"] == start_date]
+
+    def _calc_reward(self):
         pass
 
-    def step():
+    def _determine_end(self):
         pass
-
-
-async def main():
-    game = GameEnvironmnet("SPY", "2022-01-01")
-    s_price, o_contracts, o_prices = await game.pull_game_price_data()
-    print(s_price)
 
 
 # NOTE: this could be used for the logic of pulling the prices to be considered for each step
@@ -104,4 +139,4 @@ async def main():
     """
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    pass
