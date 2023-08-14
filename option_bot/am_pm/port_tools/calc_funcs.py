c58ac9288127e85217c3826a1ad97729e73e2ad7
jhirschibar <jhirschibar@gmail.com>
2023-08-14 08:47:11 -0400

    feat: finished state initialization

--- option_bot/am_pm/port_tools/calc_funcs.py
+++ option_bot/am_pm/port_tools/calc_funcs.py
@@ -5,18 +5,21 @@ from numpy.lib.stride_tricks import sliding_window_view
 def calc_log_returns(prices: np.ndarray) -> np.ndarray:
     """Calculates log returns for a given array of prices.
     Return index will be one less than the input price index."""
-    return np.log(prices[1:] / prices[:-1])
+    t = np.log(prices[1:] / prices[:-1])
+    return np.insert(t, obj=0, values=0, axis=0)
 
 
 def calc_pct_returns(prices: np.ndarray) -> np.ndarray:
     """Calculates percentage returns for a given array of prices.
     Return index will be one less than the input price index."""
-    return prices[1:] / prices[:-1] - 1
+    t = prices[1:] / prices[:-1] - 1
+    return np.insert(t, obj=0, values=0, axis=0)
 
 
 def calc_hist_volatility(returns: np.ndarray, period: int) -> np.ndarray:
     """Calculates historical volatility for a given array of returns."""
-    return np.std(sliding_window_view(returns, period), axis=1) * np.sqrt(period)
+    t = np.std(sliding_window_view(returns, period), axis=1) * np.sqrt(period)
+    return np.insert(t, obj=[0 for i in range(period)], values=0, axis=0)
 
 
 def calc_correlation(array_1: np.ndarray, array_2: np.ndarray) -> np.ndarray:
