83149d097ea28477a7488cdb312b868fe85324e6
jhirschibar <jhirschibar@gmail.com>
2023-08-26 08:48:50 -0400

    feat: beginning to train model

--- option_bot/rl_agent/train.py
+++ option_bot/rl_agent/train.py
@@ -1,5 +1,17 @@
-def train_agent():
-    pass
+from rl_agent.game_environment import GameEnvironment
+
+
+def train_agent(ticker: str, start_date: str, num_positions: int):
+    """
+    Script to train the DQN network and the learning agent
+
+    Args:
+        ticker (str): ticker symbol
+        start_date (str): start date for training data. Will use all price data from start date to present
+        num_positions (int): number of positions to hold in the portfolio
+    """
+    env = GameEnvironment(ticker, start_date, num_positions=num_positions)
+    env.prepare_state_data()
 
 
 def optimize_with_replay():
