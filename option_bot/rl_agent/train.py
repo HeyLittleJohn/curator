93ef2a63e264835c0b6ceb82c36e040741405c6b
jhirschibar <jhirschibar@gmail.com>
2023-08-28 08:40:06 -0400

    feat: training script with reward threshold

--- option_bot/rl_agent/train.py
+++ option_bot/rl_agent/train.py
@@ -1,4 +1,8 @@
+from torch.optim import Adam
+
 from rl_agent.game_environment import GameEnvironment
+from rl_agent.agent import DQN_Network
+from rl_agent.constants import EPISODES, BATCH_SIZE, MEMORY_MAX, DEVICE
 
 
 def train_agent(ticker: str, start_date: str, num_positions: int):
@@ -12,6 +16,17 @@ def train_agent(ticker: str, start_date: str, num_positions: int):
     """
     env = GameEnvironment(ticker, start_date, num_positions=num_positions)
     env.prepare_state_data()
+    model = DQN_Network(actions_dim=len(env.actions), state_dim=len(env.feature_cols))
+    sgd = Adam(model.parameters(), lr=model.alpha)
+
+    for i in range(EPISODES):
+        state = env.reset()
+        reward = 0
+        while not env.end:
+            action = model.choose_action(state)
+            next_state, reward = env.step(action)
+            model.rewards.append(reward)
+            state = next_state
 
 
 def optimize_with_replay():
