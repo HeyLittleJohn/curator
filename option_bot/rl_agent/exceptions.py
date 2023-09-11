10dc555002a108d2adc9845319a6de2ab9596885
jhirschibar <jhirschibar@gmail.com>
2023-09-11 08:47:37 -0400

    fix: try/except for empty init

--- option_bot/rl_agent/exceptions.py
+++ option_bot/rl_agent/exceptions.py
@@ -8,3 +8,9 @@ class InvalidReset(Exception):
     """Exception raised when the reset() function is called when the state data has not been loaded"""
 
     pass
+
+
+class EmptyInit(Exception):
+    """Exception raised when the init_random_positions() func pics a ticker with no state data"""
+
+    pass
