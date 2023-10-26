8694351e817e1c70a9b7c54726a6c3c32abb5801
jhirschibar <jhirschibar@gmail.com>
2023-10-26 09:04:09 -0400

    refactor: updated torch dependencies

--- option_bot/rl_agent/train.py
+++ option_bot/rl_agent/train.py
@@ -26,12 +26,14 @@ async def train_agent(ticker: str, start_date: str, num_positions: int):
     env = GameEnvironment(ticker, start_date, num_positions=num_positions)
     await env.prepare_state_data()
     model = DQN_Network(actions_dim=len(env.actions_labels), state_dim=len(FEATURE_COLS))
+    model.apply(weights_init)
     model.to(DEVICE)
     sgd = Adam(model.parameters(), lr=model.alpha)
     memory = Memories(MEMORY_MAX)
 
     done = False
     episode = 0
+    new_mem_length = 0
     while episode <= EPISODES:
         state, game_positions = env.reset()
         reward = 0
@@ -57,18 +59,20 @@ async def train_agent(ticker: str, start_date: str, num_positions: int):
             reward = calc_port_return_from_positions(game_positions)
             # NOTE: "reward" here is only used to check progress, not for optimization
 
-            if len(memory.replay_memory) >= BATCH_SIZE:
-                optimize_with_replay(model, memory, sgd, BATCH_SIZE)
-
             state = next_state
             game_positions = new_game_positions
 
-        if len(memory.replay_memory) >= BATCH_SIZE:
+        # backward pass
+        if len(memory.replay_memory) >= BATCH_SIZE and (
+            new_mem_length != len(memory.replay_memory) or len(memory.replay_memory) == MEMORY_MAX
+        ):  # if memory is maxed but no new transitions were added, may need to end the training
             print(f"Episode: {episode}, Reward: {reward}, Memories: {len(memory.replay_memory)}")
+            optimize_with_replay(model, memory, sgd, BATCH_SIZE)
             model.decay_epsilon()
             model.gamma_update(episode)
             done = model.check_progress(reward, episode)
             episode += 1
+            new_mem_length = len(memory.replay_memory)
         if done:
             break
     return model
@@ -101,3 +105,14 @@ def optimize_with_replay(model, memories, optimizer, batch_size):
     loss.backward()
     optimizer.step()
     return
+
+
+@torch.no_grad()
+def weights_init(model):
+    classname = model.__class__.__name__
+    if classname.find("Linear") != -1:
+        # get the number of the inputs
+        n = model.in_features
+        y = 1.0 / np.sqrt(n)
+        model.weight.normal_(0.0, 0.1)
+        model.bias.fill_(0)
